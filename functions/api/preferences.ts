7f8bc29856e1d2a8e03918bcfe30dad321650fb6
Varun Khalate <varunk@Varuns-MacBook-Pro.local>
2024-10-25 04:00:56 -0500

    feat: add preferences api to set cookie

---
+++ functions/api/preferences.ts
@@ -0,0 +1,13 @@
+export async function onRequest() {
+    const preferenceData = JSON.stringify({
+        "displayDuration": 5000,
+        "preferredTypes": ["alert", "info"],
+    });
+
+    const response = new Response();
+    const newCookie = `preferences=${preferenceData}`;
+
+    response.headers.set("Set-Cookie",newCookie)
+  
+    return response;
+}
