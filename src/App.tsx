1a958cbaf9d455fd1a03be40d2a182dc66531725
Varun Khalate <varunk@Varuns-MacBook-Pro.local>
2024-10-25 10:17:56 -0500

    add labels to form fields

--- src/App.tsx
+++ src/App.tsx
@@ -130,7 +130,9 @@ function formatTimestamp(timestamp:number) {
           <form id="notification-form"  onSubmit={handleSubmit} className='parent-left '>
             <div className='notif-form'>
               <div className='notif-form-title'>Create Notification</div>
+            <label htmlFor="notifcation-message">Enter text</label>
             <textarea className='notif-form-textarea' placeholder='Message' id="notification-message" required value={formData.text} onChange={(e) => onFormDataChange(e, 'text') } />
+            <label htmlFor='notification-type'>Choose type</label>
             <select id="notification-type" value={formData.type} required onChange={(e) => onFormDataChange(e, 'type') }>
             {/* <option value="" disabled selected>
               Choose type
