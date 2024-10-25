2e884d9fc894ec11c9d72ff9fb6c619e25f4c48a
Varun Khalate <varunk@Varuns-MacBook-Pro.local>
2024-10-25 06:30:55 -0500

    feat: add ai api endpoint

--- functions/api/ai.ts
+++ functions/api/ai.ts
@@ -1,19 +1,22 @@
-export interface Env {
-    AI: any;
-  }
-  
-  export default {
-    async fetch(request, env): Promise<Response> {
-  
-      const messages = [
-        { role: "system", content: "You are a friendly assistant" },
-        {
-          role: "user",
-          content: "What is the origin of the phrase Hello, World",
-        },
-      ];
-      const response = await env.AI.run("@cf/meta/llama-3-8b-instruct", { messages });
-  
-      return Response.json(response);
-    },
-  } ;
+export async function onRequest(context) {
+  const { request, env } = context;
+  const req = await request.json();
+  const { text } = req;
+
+  const stream = await env.AI.run("@cf/meta/llama-3-8b-instruct", {
+    stream: false,
+    max_tokens: 512,
+    messages: [
+      { role: "system", content: "Classify the given notification text into one of these categories: finance, weather, health, or technology. The text will clearly belong to one of these categories. Respond with only the category name in lowercase."},
+      { role: "user", content: text},
+    ],
+  });
+
+
+  let res = {category: stream.response};
+
+  return new Response(JSON.stringify(res), {
+    headers: { "content-type": "application/json" },
+  });
+
+}
