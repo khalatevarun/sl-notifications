037db1d9717705ccd3c63f82ff1d451dada87b5d
Varun Khalate <varunk@Varuns-MacBook-Pro.local>
2024-10-25 01:35:04 -0500

    feat: add form to input notification content from user and make a POST api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,41 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react';
 
 function App() {
+
+  const [formData, setFormData] = useState({
+    text: '',
+    type: ''
+  });
+
 
   const getNotification = async() =>{
    await fetch('/api/notifications',{
       method:'GET',
     }).then((response)=> response.json()).then((res)=>console.log(res));
 
-
-    
   }
 
-  const addNotifications = async() => {
+  const addNotifications = async(data:any) => {
+
+    const {type, text} = data;
+
+    const notification = {
+      type,
+      content: {
+        text,
+      },
+      read: false
+    }
    await fetch('/api/notifications',{
       method:'POST',
-      body: JSON.stringify({
-        "type": "alert",
-        "content": {
-          "text": "New notification alerts!"
-        },
-        "read": false
-      }),
+      body: JSON.stringify(notification),
     }).then((response)=> response.json()).then((res)=>console.log(res));
     
-
+    setFormData({
+      text:'',
+      type: ''
+    })
   }
 
   const deleteNotifications = async() => {
@@ -47,20 +57,50 @@ function App() {
         "read": "not read"
       }),
     }).then((response)=> response.json()).then((res)=>console.log(res));
+
+    console.log("REACHED HERE");
+
+   
     
   }
 
+
+  const onFormDataChange = (e: React.ChangeEvent<HTMLTextAreaElement|HTMLSelectElement>, key:string) => {
+    const value = e.target.value;
+
+    setFormData({...formData, [key]: value});
+  }
+
+  const handleSubmit = (e:any) => {
+      e.preventDefault();
+      addNotifications(formData);
+      
+  }
+
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='parent-container'>
+        <div className='parent-left'>
+          <div>Create Notification</div>
+          <form id="notification-form" onSubmit={handleSubmit} className='create-notification-form'>
+            <textarea id="notification-message" required value={formData.text} onChange={(e) => onFormDataChange(e, 'text') } />
+            <select id="notification-type" value={formData.type} required onChange={(e) => onFormDataChange(e, 'type') }>
+            <option value="" disabled selected>
+              Select type
+             </option>
+              <option value="alert">Alert</option>
+              <option value="info">Info</option>
+              <option value="success">Success</option>
+            </select>
+            <button id="send-notification-btn" type='submit'>Send</button>
+          </form>
+        </div>
+        <div>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
+      
       <div className="card">
         <button onClick={getNotification}>
           Get Notifications
@@ -74,9 +114,7 @@ function App() {
         <button onClick={deleteNotifications}>
           Delete Notifications
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
