50004c193c4ea496fdc71d9b2fc13725e7ceb737
Varun Khalate <varunk@Varuns-MacBook-Pro.local>
2024-10-25 08:08:19 -0500

    remove other flags from cookies

--- functions/api/preferences.ts
+++ functions/api/preferences.ts
@@ -5,7 +5,7 @@ export async function onRequest() {
     });
 
     const response = new Response();
-    const newCookie = `preferences=${preferenceData}; Path=/; HttpOnly`;
+    const newCookie = `preferences=${preferenceData}`;
 
     response.headers.set("Set-Cookie",newCookie);
     response.headers.set("Content-Type","application/json");
