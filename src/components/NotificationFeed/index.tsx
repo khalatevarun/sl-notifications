156836eda303631fe2a0ec2b81323b32d4f49b03
Varun Khalate <varunk@Varuns-MacBook-Pro.local>
2024-10-26 14:43:56 -0500

    add new list visualiser

--- src/components/NotificationFeed/index.tsx
+++ src/components/NotificationFeed/index.tsx
@@ -1,31 +1,47 @@
-import { Notification } from "../../utility/types";
+import AutoSizer from "react-virtualized-auto-sizer";
+import { FixedSizeList as List } from "react-window";
 
-import './notificationFeed.css'
+import { Notification } from "../../utility/types";
+import './notificationFeed.css';
 
 type Props = {
     notifications: Notification[]
 }
 
-const NotificationFeed = (props:Props) => {
+const NotificationFeed = (props: Props) => {
 
-    const { notifications }= props;
+    const { notifications } = props;
 
-   return (
-     <div id='notification-feed' className='parent-right'> 
-            {notifications.map((notification: Notification)=>(
-              <div 
-                className={`notification-card ${notification?.type}-bg`}
-                >
-              <p className='notification-message'>
-                {notification?.content?.text}
-              </p>
-              <div className="notification-timestamp">  
-                {notification?.timestamp}
-              </div>
-              </div>
-            ))}
+    const Row = ({ index, style }: { index: number; style: React.CSSProperties }) => (
+        <div
+            className={`notification-card ${notifications[index]?.type}-bg`}
+            style={{ ...style, marginBottom: index === notifications.length - 1 ? 0 : '10px' }}
+        >
+            <p className='notification-message'>
+                {notifications[index]?.content?.text}
+            </p>
+            <div className="notification-timestamp">
+                {notifications[index]?.timestamp}
+            </div>
         </div>
-   )
+    );
+
+    return (
+        <div id='notification-feed' className='parent-right'>
+            <AutoSizer>
+                {({ height, width }) => (
+                    <List
+                        height={height}
+                        width={width}
+                        itemSize={80} // 70px card + 10px gap
+                        itemCount={notifications.length}
+                    >
+                        {Row}
+                    </List>
+                )}
+            </AutoSizer>
+        </div>
+    );
 }
 
 export default NotificationFeed;
