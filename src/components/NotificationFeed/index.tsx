4180a4c43e893d52af2e58ae39c87e90182f4172
Varun Khalate <varunk@Varuns-MacBook-Pro.local>
2024-10-26 20:56:56 -0500

    add react list virutalisation

--- src/components/NotificationFeed/index.tsx
+++ src/components/NotificationFeed/index.tsx
@@ -1,5 +1,8 @@
-import { formatTimestamp } from '../../utility/helper';
+import AutoSizer from 'react-virtualized-auto-sizer';
+import { FixedSizeList as List } from 'react-window';
+
 import { Notification } from '../../utility/types';
+import { formatTimestamp } from '../../utility/helper';
 
 import './notificationFeed.css';
 
@@ -10,16 +13,39 @@ type Props = {
 const NotificationFeed = (props: Props) => {
   const { notifications } = props;
 
+  const Row = ({
+    index,
+    style,
+  }: {
+    index: number;
+    style: React.CSSProperties;
+  }) => (
+    <div style={style}>
+      <div className={`notification-card ${notifications[index]?.type}-bg`}>
+        <p className="notification-message">
+          {notifications[index]?.content?.text}
+        </p>
+        <div className="notification-timestamp">
+          {formatTimestamp(notifications[index]?.timestamp)}
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <div id="notification-feed" className="parent-right">
-      {notifications.map((notification: Notification) => (
-        <div className={`notification-card ${notification?.type}-bg`}>
-          <p className="notification-message">{notification?.content?.text}</p>
-          <div className="notification-timestamp">
-            {formatTimestamp(notification?.timestamp)}
-          </div>
-        </div>
-      ))}
+      <AutoSizer>
+        {({ height, width }) => (
+          <List
+            height={height}
+            width={width}
+            itemSize={80} // 70px for height + 10px for the gap
+            itemCount={notifications.length}
+          >
+            {Row}
+          </List>
+        )}
+      </AutoSizer>
     </div>
   );
 };
